eda2e29f634946f55733a7ddc99b0519d7179ed9
nitin <nitinsachdeva0202@gmail.com>
2023-10-04 11:55:43 +0530

    small change

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ const dotenv = require("dotenv");
 dotenv.config();
 const cors = require("cors");
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: '*',
     methods: ['GET', 'POST', 'PUT', 'DELETE']
 }));
 
